b48626f5d9520c1053e08a2539c592b8e3001605
Cecil van der Watt <cecil.vanderwatt@stanchionpayments.com>
2025-06-22 14:43:13 +0200

    Initial commit: swagger spec and swagger generation, basic integration module that copies and starts up the application

---
+++ README.md
@@ -0,0 +1,1 @@
+Family API checker assignment.
---
+++ src/main/java/com/pink/family/assignment/api/controller/PersonsController.java
@@ -0,0 +1,16 @@
+package com.pink.family.assignment.api.controller;
+
+import com.pink.family.api.rest.server.model.Person;
+import com.pink.family.api.rest.server.reference.PersonsApi;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * The controller for the Persons API methods i.e. /persons/**
+ */
+public class PersonsController implements PersonsApi {
+
+    @Override
+    public ResponseEntity<Void> personsHasPartnerChildrenCheckPost(Person person) {
+        return null;
+    }
+}
